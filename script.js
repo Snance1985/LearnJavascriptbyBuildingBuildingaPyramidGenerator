c601ce948974c936da2db249b2012e827be7b651
Sean <131737726+Snance1985@users.noreply.github.com>
2024-05-21 08:35:32 -0700

    "Added comments about using const in for-of loops and string concatenation; modified for loop to concatenate row values to result variable"

--- script.js
+++ script.js
@@ -188,6 +188,14 @@ for (const value of iterable) {
 }
 
 Note that you can use const because the variable only exists for a single iteration, not during the entire loop.
+
+Remember in your previous loop that you used the addition operator + to increase the value of i by 1.
+
+You can do a similar thing with a string value, by appending a new string to an existing string.
+For example, hello = hello + " World"; would add the string " World" to the existing string stored in the hello variable.
+This is called concatenation.
+
+
 */
 
 const character = "#";
@@ -199,5 +207,5 @@ for(let i = 0; i < count; i= i +1) {
 let result = "";
 console.log(result);
 for (const row of rows) {
-
+    result = result + row;
 }
