5dde0b455c4f02997e13b1b4044012f1351aef87
Sean <131737726+Snance1985@users.noreply.github.com>
2024-05-25 19:27:23 -0700

    "Added comments about JavaScript variable scope and modified padRow function to prefix character to name"

--- script.js
+++ script.js
@@ -264,13 +264,29 @@ function demo(name) {
 }
 demo("Naomi");
 
+Variables in JavaScript are available in a specific scope.
+In other words, where a variable is declared determines where in your code it can be used.
+
+The first scope is the global scope.
+Variables that are declared outside of any "block" like a function or for loop are in the global scope.
+Your character, count, and rows variables are all in the global scope.
+
+When a variable is in the global scope, a function can access it in its definition.
+Here is an example of a function using a global title variable:
+
+const title = "Professor ";
+function demo(name) {
+  return title + name;
+}
+demo("Naomi")
+
 */
 
 const character = "#";
 const count = 8;
 const rows = [];
 function padRow(name) {
-  return name;
+  return character + name;
 }
 const call = padRow("Sean");
 console.log(call);
